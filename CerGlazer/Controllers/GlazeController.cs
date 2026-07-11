b14deee1bab1dd820d191c237cb5a91ead4007da
MyJanusLife <JacquelineLove@13>
2026-07-11 18:57:35 -0400

    Updates to code files to include new logic for glaze recipe display.

--- CerGlazer/Controllers/GlazeController.cs
+++ CerGlazer/Controllers/GlazeController.cs
@@ -1,24 +1,66 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using CerGlazer.Models;
 
 namespace CerGlazer.Controllers
 {
-    public class GlazeController : Controller
+    public class GlazesController : Controller
     {
+
         // GET: GlazeController
-        public ActionResult Index()
+        public IActionResult Index()
         {
             return View();
         }
 
+        /// <summary>
+        /// List of glaze recipes with their details, including ingredients and application methods.
+        /// </summary>
+        /// <returns>IActionResult</returns>
+        public IActionResult GlazeRecipes()
+        {
+            var glazeRecipes = new List<GlazeRecipe>
+            {
+                new GlazeRecipe
+                {
+                    Name = "Celadon",
+                    Cone = "5",
+                    Color = "Green",
+                    Ingredients = new List<GlazeMaterial>
+                    {
+                        new GlazeMaterial { Name = "Feldspar", Amount = 50 },
+                        new GlazeMaterial { Name = "Kaolin", Amount = 30 },
+                        new GlazeMaterial { Name = "Silica", Amount = 20 }
+                    },
+                    PreferredApplicationMethod = Models.Enums.ApplicationMethods.Dip,
+                    Notes = "A classic green glaze."
+                },
+                new GlazeRecipe
+                {
+                    Name = "Shino",
+                    Cone = "6",
+                    Color = "Orange",
+                    Ingredients = new List<GlazeMaterial>
+                    {
+                        new GlazeMaterial { Name = "Feldspar", Amount = 40 },
+                        new GlazeMaterial { Name = "Kaolin", Amount = 40 },
+                        new GlazeMaterial { Name = "Iron Oxide", Amount = 20 }
+                    },
+                    PreferredApplicationMethod = Models.Enums.ApplicationMethods.Brush,
+                    Notes = "A traditional Japanese glaze."
+                }
+            };
+            return View(glazeRecipes);
+        }
+
         // GET: GlazeController/Details/5
-        public ActionResult Details(int id)
+        public IActionResult Details(int id)
         {
             return View();
         }
 
         // GET: GlazeController/Create
-        public ActionResult Create()
+        public IActionResult Create()
         {
             return View();
         }
@@ -26,7 +68,7 @@ namespace CerGlazer.Controllers
         // POST: GlazeController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Create(IFormCollection collection)
+        public IActionResult Create(IFormCollection collection)
         {
             try
             {
@@ -39,7 +81,7 @@ namespace CerGlazer.Controllers
         }
 
         // GET: GlazeController/Edit/5
-        public ActionResult Edit(int id)
+        public IActionResult Edit(int id)
         {
             return View();
         }
@@ -47,7 +89,7 @@ namespace CerGlazer.Controllers
         // POST: GlazeController/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public IActionResult Edit(int id, IFormCollection collection)
         {
             try
             {
@@ -60,7 +102,7 @@ namespace CerGlazer.Controllers
         }
 
         // GET: GlazeController/Delete/5
-        public ActionResult Delete(int id)
+        public IActionResult Delete(int id)
         {
             return View();
         }
@@ -68,7 +110,7 @@ namespace CerGlazer.Controllers
         // POST: GlazeController/Delete/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Delete(int id, IFormCollection collection)
+        public IActionResult Delete(int id, IFormCollection collection)
         {
             try
             {
--- CerGlazer/Program.cs
+++ CerGlazer/Program.cs
@@ -25,4 +25,8 @@ app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Home}/{action=Index}/{id?}");
 
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Glazes}/{action=GlazeRecipes}");
+
 app.Run();
